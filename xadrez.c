9c41724972b2e6f5054e6adc8221804cb090e59d
Elvis Vitor <167098338+ev9s@users.noreply.github.com>
2025-04-10 21:03:48 -0300

    Desafio Novato xadrez.c

--- xadrez.c
+++ xadrez.c
@@ -1,32 +1,27 @@
 #include <stdio.h>
 
-// Desafio de Xadrez - MateCheck
-// Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
-// O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
+int main(){
+    //variáveis
+    int torre = 1;
+    int bispo = 1;
+    int rainha = 1;
 
-int main() {
-    // Nível Novato - Movimentação das Peças
-    // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
+    //repetição da torre
+    for(int torre; torre <= 5; torre++ ){
+        printf("Torre: Direita.\n");
+    }
 
-    // Implementação de Movimentação do Bispo
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
+    //repetição do bispo
+    while(bispo <= 5){
+        printf("Bispo: Cima, direita.\n");
+        bispo++;
+    }
 
-    // Implementação de Movimentação da Torre
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita.
-
-    // Implementação de Movimentação da Rainha
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
-
-    // Nível Aventureiro - Movimentação do Cavalo
-    // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
-    // Um loop pode representar a movimentação horizontal e outro vertical.
-
-    // Nível Mestre - Funções Recursivas e Loops Aninhados
-    // Sugestão: Substitua as movimentações das peças por funções recursivas.
-    // Exemplo: Crie uma função recursiva para o movimento do Bispo.
-
-    // Sugestão: Implemente a movimentação do Cavalo utilizando loops com variáveis múltiplas e condições avançadas.
-    // Inclua o uso de continue e break dentro dos loops.
-
-    return 0;
+    //repetição da rainha
+    do
+    {
+        printf("Rainha: Esquerda.\n");
+        rainha++;
+    } while (rainha <= 8);
+    
 }
